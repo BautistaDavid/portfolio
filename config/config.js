20d57526a65e43a4b70481ecae280cb3de303654
David Felipe Bautista <78710558+BautistaDavid@users.noreply.github.com>
2022-06-08 23:19:30 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -101,23 +101,6 @@ export const projects = {
 }
 
 
-export const contact = {
-	title: "Get in touch",
-	description: "Coffee Chat! Please do not hesitate to schedule a meeting. Alternatively, feel free to reach out directly by email at hashirshoaeb@gmail.com.",
-	buttons: [
-		{
-			title: "Email Me",
-			link: "mailto:hashirshoaeb@gmail.com",
-			isPrimary: true,
-		},
-		{
-			title: "Schedule Meeting",
-			link: "",
-			isPrimary: false,
-		},
-	]
-}
-
 // SEARCH ENGINE 
 export const SEO = {
 	// 50 - 60 char  
