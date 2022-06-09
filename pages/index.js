6cc5036612a588bdef0e27bbe69aeca369065076
David Felipe Bautista Bernal <davidbautistab@usantotomas.edu.co>
2022-06-09 10:56:20 -0500

    test_contact_commit

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import React, { Fragment } from 'react';
 import { Nav } from '../components/Navbar';
 import { Intro, About } from '../components/Intro';
 import { Skills, Projects } from '../components/Work';
-import { Footer } from '../components/Footer';
-import { about, intro, navigation, projects, SEO, work } from '../config/config';
+import { Footer, Contact } from '../components/Footer';
+import { about, contact, intro, navigation, projects, SEO, work } from '../config/config';
 import { Header } from '../components/Header';
 
 export default function Home() {
@@ -31,6 +31,12 @@ export default function Home() {
       <Projects
         title={projects.title}
         cards={projects.cards}
+
+      />
+      <Contact
+        title={contact.title}
+        description={contact.description}
+        buttons={contact.buttons}
       />
       <Footer />
     </Fragment>
